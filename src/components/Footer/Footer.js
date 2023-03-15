828be6830572229e2714bad5e3a5b2d9cad6c474
Sasnouskaya Dziyana <progdidi@gmail.com>
2023-03-15 16:06:45 +0100

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,140 @@
+//images
+import care from './images/plantServices/care.svg';
+import renovation from './images/plantServices/renovation.svg';
+import watering from './images/plantServices/watering.svg';
+import footerLogo from './images/contacts/logo.svg';
+import facebook from "./images/social/facebook.svg";
+import instagram from "./images/social/instagram.svg";
+import linkedin from "./images/social/linkedin.svg";
+import twitter from "./images/social/twitter.svg";
+import union from "./images/social/union.svg";
+
+
+
 import './footer.scss';
 
 const Footer = () => {
     return ( 
         <>
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__services">
+                    <div className="services__item">
+                        <img src={care} alt="" className="services__item-img" />
+                        <h6 className="services__item-title">Garden Care</h6>
+                        <p className="services__item-descr">We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                    </div>
+                    <div className="services__item">
+                        <img src={renovation} alt="" className="services__item-img" />
+                        <h6 className="services__item-title">Plant Renovation</h6>
+                        <p className="services__item-descr">We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                    </div>
+                    <div className="services__item">
+                        <img src={watering} alt="" className="services__item-img" />
+                        <h6 className="services__item-title">Watering Graden</h6>
+                        <p className="services__item-descr">We are an online plant shop offering a wide range of cheap and trendy plants.</p>
+                    </div>
+
+                    <div className="newsletter">
+                        <h6 className="newsletter__title">Would you like to join newsletters?</h6>
+                        <form action="" className="newsletter__form">
+                            <input type="mail" className="newsletter__form-input" placeholder='enter your email address...'/>
+                            <button className="newsletter__form-btn">Join</button>
+                        </form>
+                        <p className="newsletter__descr">We usually post offers and challenges in newsletter. We’re your online houseplant destination. We offer a wide range of houseplants and accessories shipped directly from our (green)house to yours! </p>
+                    </div>
+                </div>
+                <div className="footer__contacts">
+                    <a href="" className="footer__logo">
+                        <img src={footerLogo} alt="logo" className="footer__logo-img" />
+                    </a>
+                    <p className="footer__contacts-item">70 West Buckingham Ave. Farmingdale, NY 11735</p>
+                    <a href="mail:ontact@greenshop.com" className="footer__contacts-item">contact@greenshop.com</a>
+                    <a href="tel:+8801911717490" className="footer__contacts-item">+88 01911 717 490</a>
+                </div>
+                <div className="footer__info">
+                    <div className="footer__info-item">
+                        <h6 className="footer__info-title">My Account</h6>
+                        <ul className="footer__info-list">
+                            <li className="footer__info-item"><a href="" className="footer__info-link">My Account</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Our stores</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Contact us</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Career</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Specials</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer__info-item">
+                        <h6 className="footer__info-title">Help & Guide</h6>
+                        <ul className="footer__info-list">
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Help Center</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">How to Buy</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Shipping & Delivery</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Product Policy</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">How to Return</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer__info-item">
+                        <h6 className="footer__info-title">Categories</h6>
+                        <ul className="footer__info-list">
+                            <li className="footer__info-item"><a href="" className="footer__info-link">House Plants</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Potter Plants</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Seeds</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Small Plants</a></li>
+                            <li className="footer__info-item"><a href="" className="footer__info-link">Accessories</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer__info-item">
+                        <h6 className="footer__info-title">Social Media</h6>
+                        <ul className="social__list">
+                            <li className="social__list-item">
+                                <a href="" className="social__list-link">
+                                    <img src={facebook} alt="facebook" className="social__list-img" />
+                                </a>
+                            </li>
+                            <li className="social__list-item">
+                                <a href="" className="social__list-link">
+                                    <img src={instagram} alt="instagram" className="social__list-img" />
+                                </a>
+                            </li>
+                            <li className="social__list-item">
+                                <a href="" className="social__list-link">
+                                    <img src={twitter} alt="twitter" className="social__list-img" />
+                                </a>
+                            </li>
+                            <li className="social__list-item">
+                                <a href="" className="social__list-link">
+                                    <img src={linkedin} alt="linkedin" className="social__list-img" />
+                                </a>
+                            </li>
+                            <li className="social__list-item">
+                                <a href="" className="social__list-link">
+                                    <img src={union} alt="union" className="social__list-img" />
+                                </a>
+                            </li>
+                        </ul>
+
+                        <h6 className="footer__info-title">We accept</h6>
+                        <ul className="payment__list">
+                            <li className="payment__list-item">
+                                <img src="" alt="" className="payment__list-img" />
+                            </li>
+                            <li className="payment__list-item">
+                                <img src="" alt="" className="payment__list-img" />
+                            </li>
+                            <li className="payment__list-item">
+                                <img src="" alt="" className="payment__list-img" />
+                            </li>
+                            <li className="payment__list-item">
+                                <img src="" alt="" className="payment__list-img" />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__bottom">
+                    <p className="footer__bottom-text">© 2021 GreenShop. All Rights Reserved.</p>
+                </div>
+            </div>
+        </footer>
         </>
      );
 }
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -17,142 +17,145 @@ const MainPage = () => {
 
         <section className="catalog">
             <div className="container">
-                <div className="catalog__aside">
-                    <div className="catalog__filter">
-                        <div className="catalog__filter-category">
-                            <h6 className="catalog__filter-title">Categories</h6>
-                            <ul className="catalog__filter-list">
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">House Plants</p>
-                                        <p className="catalog__filter-amount">(33)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Potter Plants</p>
-                                        <p className="catalog__filter-amount">(12)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Seeds</p>
-                                        <p className="catalog__filter-amount">(65)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Small Plants</p>
-                                        <p className="catalog__filter-amount">(39)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Big Plants</p>
-                                        <p className="catalog__filter-amount">(23)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Succulents</p>
-                                        <p className="catalog__filter-amount">(17)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Trerrariums</p>
-                                        <p className="catalog__filter-amount">(19)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Gardening</p>
-                                        <p className="catalog__filter-amount">(13)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Accessories</p>
-                                        <p className="catalog__filter-amount">(18)</p>
-                                    </button>                                
-                                </li>
-                            </ul>
-                        </div>
+                <div className="catalog__inner">
+                    <div className="catalog__aside">
+                        <div className="catalog__filter">
+                            <div className="catalog__filter-category">
+                                <h6 className="catalog__filter-title">Categories</h6>
+                                <ul className="catalog__filter-list">
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">House Plants</p>
+                                            <p className="catalog__filter-amount">(33)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Potter Plants</p>
+                                            <p className="catalog__filter-amount">(12)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Seeds</p>
+                                            <p className="catalog__filter-amount">(65)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Small Plants</p>
+                                            <p className="catalog__filter-amount">(39)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Big Plants</p>
+                                            <p className="catalog__filter-amount">(23)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Succulents</p>
+                                            <p className="catalog__filter-amount">(17)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Trerrariums</p>
+                                            <p className="catalog__filter-amount">(19)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Gardening</p>
+                                            <p className="catalog__filter-amount">(13)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Accessories</p>
+                                            <p className="catalog__filter-amount">(18)</p>
+                                        </button>                                
+                                    </li>
+                                </ul>
+                            </div>
 
-                        <div className="catalog__filter-category">
-                            <h6 className="catalog__filter-title">Price Range</h6>
-                            <form className="price-range__form" action="">   
-                                                             
-                                <input type="range" id="price-range" className="price-range__form-input" name="price" min="39" max="1500"/>
-                                <label for="price" className="price-range__form-label">Price: $39 - $1230</label>
-                                <input className="price-range__form-btn" type="submit" value="Filter"/>
-                            </form>
-                        </div>
+                            <div className="catalog__filter-category">
+                                <h6 className="catalog__filter-title">Price Range</h6>
+                                <form className="price-range__form" action="">   
+                                                                
+                                    <input type="range" id="price-range" className="price-range__form-input" name="price" min="39" max="1500"/>
+                                    <label for="price" className="price-range__form-label">Price: $39 - $1230</label>
+                                    <input className="price-range__form-btn" type="submit" value="Filter"/>
+                                </form>
+                            </div>
 
-                        <div className="catalog__filter-category">
-                            <h6 className="catalog__filter-title">Size</h6>
-                            <ul className="catalog__filter-list">
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Small</p>
-                                        <p className="catalog__filter-amount">(119)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Medium</p>
-                                        <p className="catalog__filter-amount">(86)</p>
-                                    </button>                                
-                                </li>
-                                <li className="catalog__filter-item">
-                                    <button className="catalog__filter-btn">
-                                        <p className="catalog__filter-product">Large</p>
-                                        <p className="catalog__filter-amount">(78)</p>
-                                    </button>                                
-                                </li>
-                            </ul>
+                            <div className="catalog__filter-category">
+                                <h6 className="catalog__filter-title">Size</h6>
+                                <ul className="catalog__filter-list">
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Small</p>
+                                            <p className="catalog__filter-amount">(119)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Medium</p>
+                                            <p className="catalog__filter-amount">(86)</p>
+                                        </button>                                
+                                    </li>
+                                    <li className="catalog__filter-item">
+                                        <button className="catalog__filter-btn">
+                                            <p className="catalog__filter-product">Large</p>
+                                            <p className="catalog__filter-amount">(78)</p>
+                                        </button>                                
+                                    </li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                        <div className="banner">
+                            <img src={banner} alt="banner" className="banner__img" />
+                        </div>
+                    </div>
+                    <div className="catalog__content">
+                        <div className="catalog__top">
+                            <div className="catalog__top-buttons">
+                                <div className="catalog__top-btn">All Plants</div>
+                                <div className="catalog__top-btn">New Arrivals</div>
+                                <div className="catalog__top-btn">Sale</div>
+                            </div>
+
+                            <div className="catalog__top-sort">
+                                <p className="catalog__sort-text">Short by:</p>
+                                <select className="catalog__sort-items">
+                                    <option className="catalog__sort-items">prise</option>
+                                    <option className="catalog__sort-items">size</option>
+                                    <option className="catalog__sort-items">prise</option>
+                                    <option className="catalog__sort-items">Кукушкина</option>
+                                </select>
+                            </div>
                         </div>
                         
-                    </div>
-                    <div className="banner">
-                        <img src={banner} alt="banner" className="banner__img" />
-                    </div>
-                </div>
-                <div className="catalog__content">
-                    <div className="catalog__content-top">
-                        <div className="catalog__content-buttons">
-                            <div className="catalog__content-btn">All Plants</div>
-                            <div className="catalog__content-btn">New Arrivals</div>
-                            <div className="catalog__content-btn">Sale</div>
-                        </div>
-
-                        <div className="catalog__content-sort">
-                            <p className="catalog__sort-text">Short by:</p>
-                            <select className="catalog__sort-items">
-                                <option className="catalog__sort-items">prise</option>
-                                <option className="catalog__sort-items">size</option>
-                                <option className="catalog__sort-items">prise</option>
-                                <option className="catalog__sort-items">Кукушкина</option>
-                            </select>
-                        </div>
-                    </div>
-                    
-                    <div className="catalog__content-products">
                         <div className="catalog__items">
-                            {plants.map((plant) => {
-                                return (
-                                    <CatalogItem
-                                        key={plant.id}
-                                        title={plant.title} 
-                                        price={plant.descr}
-                                        img={plant.img} 
-                                        link={plant.link}
-                                    />
-                                )                            
-                            })}
+                            <div className="catalog__products">
+                                {plants.map((plant) => {
+                                    return (
+                                        <CatalogItem
+                                            key={plant.id}
+                                            title={plant.title} 
+                                            price={plant.descr}
+                                            img={plant.img} 
+                                            link={plant.link}
+                                        />
+                                    )                            
+                                })}
+                        </div>
+                    </div>
                     </div>
                 </div>
-                </div>
+                
             </div>
         </section>
               
